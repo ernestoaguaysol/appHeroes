aa2ed39b0a86f6a9e01d0ffdf1c3b49f811c8a27
Ernesto <ernestoaguaysol@gmail.com>
2017-11-30 15:05:12 -0300

    heroe component and view

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styles: []
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent {
 
-  constructor() { }
+  heroe: Heroe;
 
-  ngOnInit() {
+  constructor( private activatedRoute: ActivatedRoute,
+                private router: Router,
+                private _heroesService: HeroesService) {
+    this.activatedRoute.params.subscribe( params => {
+      // console.log(params['id']);
+      this.heroe = this._heroesService.getHeroe( params['id']);
+      console.log(this.heroe);
+    });
+  }
+
+  regresar() {
+    this.router.navigate(['/heroes']);
   }
 
 }
